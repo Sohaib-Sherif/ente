da83c97889ffbfbf981a4464037351eabb378771
Sohaib Sherif <triples17t@gmail.com>
2025-07-09 13:46:10 +0200

    fix(web): gallery reducer faulty code after rebase

--- web/packages/new/photos/components/gallery/reducer.ts
+++ web/packages/new/photos/components/gallery/reducer.ts
@@ -647,16 +647,6 @@ const galleryReducer: React.Reducer<GalleryState, GalleryAction> = (
                     familyData,
                     state.collections,
                 ),
-                view,
-                filteredFiles: deriveAlbumsFilteredFiles(
-                    action.files,
-                    action.trashedFiles,
-                    archivedCollectionIDs,
-                    hiddenFileIDs,
-                    state.tempDeletedFileIDs,
-                    state.tempHiddenFileIDs,
-                    view,
-                ),
             };
         }
 
@@ -829,16 +819,6 @@ const galleryReducer: React.Reducer<GalleryState, GalleryAction> = (
 
         case "markTempDeleted":
             return stateByUpdatingFilteredFiles({
-                ...state,
-                peopleState,
-                selectedPersonID: view.activePerson?.id,
-                extraVisiblePerson,
-                view,
-                filteredFiles,
-            };
-        }
-        case "markTempDeleted":
-            return refreshingFilteredFilesIfShowingAlbumsOrHiddenAlbums({
                 ...state,
                 tempDeletedFileIDs: new Set(
                     [...state.tempDeletedFileIDs].concat(
